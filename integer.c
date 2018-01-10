c44a96a11a0a08ae071e2ea5a03b98c14d576c29
sowmiyasuba <31585889+sowmiyasuba@users.noreply.github.com>
2018-01-10 16:55:50 +0530

    Create integer.c

---
+++ integer.c
@@ -0,0 +1,14 @@
+#include<stdio.h>
+#include<conio.h>?
+void main()
+{
+int count=0,n;
+
+printf("Enter an number:"):
+scanf("%d",&n);
+while(n!=0)
+n/=10;
+++count;
+}
+printf("Number of digits:%d,count);
+}
