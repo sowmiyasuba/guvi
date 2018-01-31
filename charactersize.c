ada2584214e74ae76f199684cdf0d33c9f408df4
sowmiyasuba <31585889+sowmiyasuba@users.noreply.github.com>
2018-01-31 15:50:05 +0530

    Update charactersize.c

--- charactersize.c
+++ charactersize.c
@@ -1,11 +1,12 @@
+#include<stdio.h>
 int main()
 {
     char a[25];
     int d;
-    printf("Enter the word:");
+    
     gets(a);
     d=strlen(a);
-    printf("The num of character in given word is:%d",d);
+    printf("%d",d);
 
 
 
