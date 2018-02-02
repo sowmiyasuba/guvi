33cc50c529a136fec57cb8c6439288690686a189
sowmiyasuba <31585889+sowmiyasuba@users.noreply.github.com>
2018-02-02 16:56:45 +0530

    Create numerical.c

---
+++ numerical.c
@@ -0,0 +1,33 @@
+#include<stdio.h>
+    int main()
+{
+    char    a[100];
+    int Digits,Alphabet,SpecialChar,Spaces;
+    int count;
+ 
+
+    Digits=Alphabet=SpecialChar=Spaces=0;
+ 
+    printf("Enter a string: ");
+    gets(a);
+ 
+    for(count=0;a[count]!=NULL;count++)
+    {
+ 
+        if(a[count]>='0' && a[count]<='9')
+            Digits++;
+        else if((a[count]>='A' && a[count]<='Z')||(a[count]>='a' && a[count]<='z'))
+            Alphabet++;
+        else if(a[count]==' ')
+            Spaces++;
+        else
+        SpecialChar++;
+    }
+ 
+    printf("numerical Characters: %d",Digits);
+ 
+    return 0;
+}
+
+
+
