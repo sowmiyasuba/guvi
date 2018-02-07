e27fcddd2c7e8e8fbeb0371f6ddf1353205a3485
sowmiyasuba <31585889+sowmiyasuba@users.noreply.github.com>
2018-02-07 16:04:24 +0530

    Create numrange.c

---
+++ numrange.c
@@ -0,0 +1,15 @@
+
+#include <stdio.h>
+
+int main()
+{
+    int n;
+    printf("enter the num:");
+    scanf("%d",&n);
+    if(n>=1&&n<=10)
+    printf("Yes");
+    else
+    printf("No") ;
+    
+    return 0;
+}
