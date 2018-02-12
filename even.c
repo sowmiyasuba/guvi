2f9b78e6e129a184f041ce56a52fcf8b9b8b0f1e
sowmiyasuba <31585889+sowmiyasuba@users.noreply.github.com>
2018-02-12 16:34:32 +0530

    Create even.c

---
+++ even.c
@@ -0,0 +1,23 @@
+#include <stdio.h>
+
+int main()
+{
+    int n,num=0;
+    printf("Enter the num:");
+    scanf("%d",&n);
+    
+    if(n%2==0)
+    {
+    printf("%d",n);
+    }
+    else
+    
+    {
+    num=n-1;
+    printf("%d",num);
+    }
+
+
+    return 0;
+}
+
