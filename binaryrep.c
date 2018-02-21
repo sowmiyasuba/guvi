9e00652076760b24a005dd117e82dd1a20b76f44
sowmiyasuba <31585889+sowmiyasuba@users.noreply.github.com>
2018-02-21 16:50:05 +0530

    Create binaryrep.c

---
+++ binaryrep.c
@@ -0,0 +1,16 @@
+#include<stdio.h>
+void main()
+{
+    char str[100];
+    int n,i;
+    printf("Enter the string:");
+    scanf("%s",&str[i]);
+    if(str[i]=='0'||str[i]=='1')
+    {
+        printf("binary representation");
+    }
+    else
+    {
+        printf("not an binary representation");
+    }
+}
