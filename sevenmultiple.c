ffbef74867b517b39e0def6ffb565bbb65b80b9e
sowmiyasuba <31585889+sowmiyasuba@users.noreply.github.com>
2018-02-26 16:58:22 +0530

    Create sevenmultiple.c

---
+++ sevenmultiple.c
@@ -0,0 +1,14 @@
+#include<stdio.h>
+int main()
+{
+    int num;
+    scanf("%d",&num);
+    
+        if(num%7==0)
+        {
+            printf("yes");
+            
+        }
+        return 0;
+  
+}
